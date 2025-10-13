0bafa8221c5862cc1d011aaa410e8c7857cd9a51
san ats <san@gmail.com>
2025-10-12 19:00:34 -0500

    Eliminar Backend de la rama frontend

--- backend/authentication/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from authentication.models import User
-
-# Register your models here.
-class UserAdmin(admin.ModelAdmin):
-    list_display = ('id', 'username', 'email', 'is_staff', 'is_active')
